06011adf3b97e3f4becad2c7b12aaf8f8797c1eb
Saad Khan <ranasaad0799@gmail.com>
2023-08-27 21:41:49 +0500

    database version and version code updated

--- app/src/main/java/com/droidgeeks/expensemanager/data/local/AppDatabase.kt
+++ app/src/main/java/com/droidgeeks/expensemanager/data/local/AppDatabase.kt
@@ -7,7 +7,7 @@ import com.droidgeeks.expensemanager.data.local.model.Transaction
 
 @Database(
     entities = [Transaction::class],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
