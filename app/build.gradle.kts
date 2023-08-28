e4be31fdb3a4954d3fff0b0b1be8b15a2d531e6f
Saad Khan <ranasaad0799@gmail.com>
2023-08-28 22:21:04 +0500

    refactoring

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,18 +42,20 @@ android {
                 getDefaultProguardFile("proguard-android.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("debug")
 
         }
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
+    namespace = "com.droidgeeks.expensemanager"
 
 }
 
